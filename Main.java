a92dbeb999534a7b254fdc91d9661f162b58c175
Flavio Chiarappa <chiarappa_flavio@yahoo.it>
2024-06-14 10:18:34 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Notifica[] notifiche = {
-                new Email("email@example.com", "Questo è un messaggio email"),
-                new Sms("+123456789", "Questo è un messaggio SMS"),
-                new NotificaApp("utente123", "Questo è un messaggio in-app")
+                new Email("flavio@example.com", "Questo è un messaggio email"),
+                new Sms("123456789", "Questo è un messaggio SMS"),
+                new NotificaApp("flavio123", "Questo è un messaggio in-app")
         };
 
         for (Notifica notifica : notifiche) {
